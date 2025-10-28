847102e4da830910ee13a9a56ab4e3927fc4a727
mephisto-inc <230451722+mephisto-inc@users.noreply.github.com>
2025-10-28 22:54:09 +0000

    Recriando git

---
+++ src/main/java/com/aulasbackend/grupoa/projeto_grupo_a/ProjetoGrupoAApplication.java
@@ -0,0 +1,13 @@
+package com.aulasbackend.grupoa.projeto_grupo_a;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoGrupoAApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoGrupoAApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/aulasbackend/grupoa/projeto_grupo_a/ProjetoGrupoAApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aulasbackend.grupoa.projeto_grupo_a;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoGrupoAApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
