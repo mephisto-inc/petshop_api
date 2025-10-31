09e190f71dba7f598e1c88e53056aeeed0a6dcd8
mephisto-inc <230451722+mephisto-inc@users.noreply.github.com>
2025-10-31 09:04:56 +0000

    Atualizando nome do projeto para petshop_api

---
+++ src/main/java/br/aulasbackend/petshop_api/PetshopApiApplication.java
@@ -0,0 +1,13 @@
+package br.aulasbackend.petshop_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetshopApiApplication {
+
+    static void main(String[] args) {
+        SpringApplication.run(PetshopApiApplication.class, args);
+    }
+
+}
